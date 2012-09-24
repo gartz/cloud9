3c73c65398016bf366887d1d3bdea976be37d89e
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2012-09-24 03:31:18 -0300

    Added script to be used in the /usr/local/bin/cloud9 that opens in the current folder as default with forever monitor

--- bin/forevercloud9.sh
+++ bin/forevercloud9.sh
@@ -1,41 +1,42 @@
 #!/bin/sh
 
+HERE=`pwd`
 NODE="node"
-CMD="$0"
-CMD_DIR=`dirname "$CMD"`
 FOREVER_NODE="node"
 FOREVER_BIN="node_modules/forever/bin/forever"
+CMD="$0"
+CMD_DIR=`dirname "$CMD"`
 cd "$CMD_DIR/.."
 
 make update
 
 case `uname -a` in
 Linux*x86_64*)  echo "Linux 64 bit"
-   $FOREVER_NODE $FOREVER_BIN -v -c "$NODE" server.js "$@" -a x-www-browser
+   $FOREVER_NODE $FOREVER_BIN -v -c "$NODE" server.js "$@" -a x-www-browser -w "$HERE"
     ;;
 
 Linux*i686*)  echo "Linux 32 bit"
-    $FOREVER_NODE $FOREVER_BIN -v -c "$NODE" server.js "$@" -a x-www-browser
+    $FOREVER_NODE $FOREVER_BIN -v -c "$NODE" server.js "$@" -a x-www-browser -w "$HERE"
     ;;
 
 Darwin*)  echo  "OSX"
-    "$NODE" server.js "$@" -a open
+    "$NODE" server.js "$@" -a open -w "$HERE"
     ;;
 
 FreeBSD*64*) echo "FreeBSD 64 bit"
-    "$NODE" server.js "$@" -a open
+    "$NODE" server.js "$@" -a open -w "$HERE"
     ;;
 
 CYGWIN*)  echo  "Cygwin"
-    "$NODE" server.js "$@" -a "cmd /c start"
+    "$NODE" server.js "$@" -a "cmd /c start" -w "$HERE"
     ;;
 
 MING*)  echo  "MingW"
-    "$NODE" server.js "$@" -a "cmd /c start"
+    "$NODE" server.js "$@" -a "cmd /c start" -w "$HERE"
     ;;
 
 SunOS*)  echo  "Solaris"
-    "$NODE" server.js "$@"
+    "$NODE" server.js "$@" -w "$HERE"
     ;;
 
 
